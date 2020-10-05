8125e61e906ced06e9d86918d184d571c06e7299
Ryan Gautier <ryangautier2@gmail.com>
2020-10-05 13:36:08 -0500

    create course schema

---
+++ models/course.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const courseSchema = new Schema({
+  title: { type: String, required: true },
+  department: { type: String, required: true },
+  courseNumber: { type: String, required: true },
+  term: { type: String, required: true },
+  classCode: { type: Number, required: true },
+  level: { type: String, required: true },
+  professor: {
+    type: Schema.Types.ObjectId,
+    ref: "Professor"
+  },
+  capacity: { type: Number, required: true },
+  numberRegistered: { type: Number, required: true },
+  finalDate: { type: Date, required: true },
+  status: { type: String, required: true },
+  days: [
+    {
+      type: String,
+      required: true
+    }
+  ],
+  startTime: { type: Date, required: true },
+  endTime: { type: Date, required: true },
+  ratings: [
+    { type: Number }
+  ],
+  usersRated: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    }
+  ],
+  textbook: { type: String }
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+module.exports = Course;
