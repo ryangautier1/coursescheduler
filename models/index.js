15d42f6c16b426cd10ffabe36e1d6e4285e3b7ee
Ryan Gautier <ryangautier2@gmail.com>
2020-10-05 11:27:01 -0500

    begin models

---
+++ models/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  User: require("./user.js"),
+  Course: require("./course.js"),
+  Professor: require("./professor.js")
+};
---
+++ models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const SALT_WORK_FACTOR = 10;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    registered: [
+      {
+          type: Schema.Types.ObjectId,
+          ref: "Course"
+      }
+    ],
+    username: {
+      type: String,
+      unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        validate: [({ length }) => length >= 6, "Password should be longer."]
+    },
+    userType: {
+        type: String,
+        default: "student"
+    },
+    amountOwed : {
+      type: Number
+    }
+
+});
+
+userSchema.pre("save", async function save(next) {  
+    // only hash the password if it has been modified (or is new)
+    if (!this.isModified("password")) {
+        return next();
+    }
+    try {
+        // generate a salt
+        const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+        // hash the password along with the new salt
+        this.password = await bcrypt.hash(this.password, salt);
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+});
+
+userSchema.methods.validatePassword = async function validatePassword(data) {
+    return bcrypt.compare(data, this.password); 
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
