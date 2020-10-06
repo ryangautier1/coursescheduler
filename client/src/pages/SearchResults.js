945f03c0825347c5548c5272006d312f8f70d232
Alvin Galit <aroblesgalit@gmail.com>
2020-10-06 11:43:25 -0500

    Created SearchResults page

---
+++ client/src/pages/SearchResults.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function SearchResults() {
+    return (
+        <div className='container-fluid'>
+            <div className='row'>
+                <div className='col col-sm-2'>
+                    Sidebar
+                </div>
+                <div className='col col-sm-10'>
+                    Main
+                </div>
+            </div>
+        </div>
+    )
+}
