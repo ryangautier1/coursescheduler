610599e3fff849bc5ea201fba2c0774fe57a2e41
Ryan Gautier <ryangautier2@gmail.com>
2020-10-07 14:04:58 -0500

    set active nav on click

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 import './navbar.css';
 
 function Navbar() {
+  const [navState, setNavState] = useState("/");
+
   return (
     <nav className="navbar  navbar-dark navbar-expand-lg text-light">
       <div className="navbar-brand mr-16">Course Scheduler</div>
@@ -10,24 +13,36 @@ function Navbar() {
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
-          <li className="nav-item active">
-            <div className="nav-link">Search <span className="sr-only">(current)</span></div>
-          </li>
-          <li className="nav-item">
-            <div className="nav-link">Planner</div>
-          </li>
-          <li className="nav-item">
-            <div className="nav-link">Dashboard</div>
-          </li>
-          <li className="nav-item">
-            <div className="nav-link">Degree Plan</div>
-          </li>
-          <li className="nav-item">
-            <div className="nav-link">Register</div>
-          </li>
-          <li className="nav-item">
-            <div className="nav-link">Help</div>
-          </li>
+          <Link to="/" onClick={() => {setNavState("/")}}>
+            <li className={navState === "/" ? "nav-item active" : "nav-item"}>
+              <div className="nav-link">Search</div>
+            </li>
+          </Link>
+          <Link to="/planner" onClick={() => {setNavState("/planner")}}>
+            <li className={navState === "/planner" ? "nav-item active" : "nav-item"}>
+              <div className="nav-link">Planner</div>
+            </li>
+          </Link>
+          <Link to="/dashboard" onClick={() => {setNavState("/dashbaord")}}>
+            <li className={navState === "/dashboard" ? "nav-item active" : "nav-item"}>
+              <div className="nav-link">Dashboard</div>
+            </li>
+          </Link>
+          <Link to="/degreeplan" onClick={() => {setNavState("/degreeplan")}}>
+            <li className={navState === "/degreeplan" ? "nav-item active" : "nav-item"}>
+              <div className="nav-link">Degree Plan</div>
+            </li>
+          </Link>
+          <Link to="/register" onClick={() => {setNavState("/register")}}>
+            <li className={navState === "/register" ? "nav-item active" : "nav-item"}>
+              <div className="nav-link">Register</div>
+            </li>
+          </Link>
+          <Link to="/help" onClick={() => {setNavState("/help")}}>
+            <li className={navState === "/help" ? "nav-item active" : "nav-item"}>
+              <div className="nav-link">Help</div>
+            </li>
+          </Link>
         </ul>
       </div>
     </nav>
