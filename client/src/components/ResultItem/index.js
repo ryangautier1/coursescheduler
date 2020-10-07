612ec834db4b7c2df34b8f2160964bfc9c7c747c
Alvin Galit <aroblesgalit@gmail.com>
2020-10-07 16:57:57 -0500

    Created component for result item

---
+++ client/src/components/ResultItem/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './resultItem.css';
+
+export default function ResultItem() {
+    return (
+        <div>
+            Item
+        </div>
+    )
+}
--- client/src/components/ResultsList/index.js
+++ client/src/components/ResultsList/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import './resultsList.css';
+import ResultItem from '../ResultItem';
 
 export default function ResultsList() {
     return (
         <div className='row'>
             <div className='list-wrapper col'>
+                {/* Status keys */}
                 <div className='status-key row'>
-                    <span>Status:</span>
-                    <ul>
-                        <li><i className='open fas fa-circle' /> Open</li>
-                        <li><i className='reserved fas fa-circle' /> Open; reserved</li>
-                        <li><i className='closed fas fa-circle' /> Closed</li>
-                        <li><i className='cancelled fas fa-circle' /> Cancelled</li>
-                    </ul>
+                    <div className='col d-flex'>
+                        <span>Status:</span>
+                        <ul>
+                            <li><i className='open fas fa-circle' /> Open</li>
+                            <li><i className='reserved fas fa-circle' /> Open; reserved</li>
+                            <li><i className='closed fas fa-circle' /> Closed</li>
+                            <li><i className='cancelled fas fa-circle' /> Cancelled</li>
+                        </ul>
+                    </div>
+                </div>
+                {/* List of results */}
+                <div className='result-items row'>
+                    <div className='col'>
+                        <ResultItem />
+                    </div>
                 </div>
             </div>
         </div>
