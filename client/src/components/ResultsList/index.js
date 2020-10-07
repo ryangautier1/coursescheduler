fe243bd5dd8bea9094234dd34abdf8d93e51ba82
Alvin Galit <aroblesgalit@gmail.com>
2020-10-07 15:40:57 -0500

    Added sort by row to results list component

--- client/src/components/ResultsList/index.js
+++ client/src/components/ResultsList/index.js
@@ -3,8 +3,22 @@ import './resultsList.css';
 
 export default function ResultsList() {
     return (
-        <div>
-            Results list
+        <div className='results-wrapper'>
+            <div className='sort-row row'>
+                <div className='col d-flex justify-content-between'>
+                    <div className='sort d-flex align-items-end'>
+                        <span>Sort by:</span>
+                        <ul>
+                            <li>Course Title</li>
+                            <li>School</li>
+                            <li>Department</li>
+                        </ul>
+                    </div>
+                    <div className='list-count'>
+                        28 Courses Found
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
