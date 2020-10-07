eb25646f973d162d8109e95173cf34cf68b916cd
Ryan Gautier <ryangautier2@gmail.com>
2020-10-07 11:46:05 -0500

    begin search page

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.css';
+
+function Home() {
+  return (
+    <div className="search-page-container">
+      <div>
+        Fall 2019
+      </div>
+    </div>
+  )
+}
+
+export default Home;
