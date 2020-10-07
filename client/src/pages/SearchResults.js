8797ab6ea6dfcc45de7778ddfd1fb58ba282b194
Alvin Galit <aroblesgalit@gmail.com>
2020-10-06 21:31:01 -0500

    Created fake filter data

--- client/src/pages/SearchResults.js
+++ client/src/pages/SearchResults.js
@@ -3,13 +3,25 @@ import './pages.css';
 import FilterButton from '../components/FilterButton';
 
 export default function SearchResults() {
+
+    const fakeFilterData = [
+        {
+            title: 'school',
+            count: 1
+        },
+        {
+            title: 'department',
+            count: 1
+        }
+    ]
+
     return (
         <div className='container-fluid'>
             <div className='row'>
                 <div className='col col-sm-2'>
                     <span className='label-small'>filters</span>
                     <div className='filter-items'>
-                        <FilterButton title='school' count={1} />
+                        { fakeFilterData.map(filter => <FilterButton title={filter.title} count={filter.count} />) }
                     </div>
                 </div>
                 <div className='col col-sm-10'>
