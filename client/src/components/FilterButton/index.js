a5ddd5e2bf6cb5351fb073c485663ec5e12e7206
Alvin Galit <aroblesgalit@gmail.com>
2020-10-07 19:57:45 -0500

    Added another field in the fake filter data

--- client/src/components/FilterButton/index.js
+++ client/src/components/FilterButton/index.js
@@ -3,15 +3,20 @@ import './filterButton.css';
 
 export default function FilterButton({ title, count }) {
     return (
-        <button
-            className='btn btn-outline-primary d-flex justify-content-between align-items-center'
-            type='button'
-            data-toggle='collapse'
-            data-target={`${title}-items`}
-            aria-expanded='false'
-            aria-controls={`${title}-items`}
-        >
-            {`${title} (${count})`} <i className="fas fa-angle-down" />
-        </button>
+        <React.Fragment>
+            <button
+                className='btn btn-outline-primary d-flex justify-content-between align-items-center'
+                type='button'
+                data-toggle='collapse'
+                data-target={`#${title}-items`}
+                aria-expanded='false'
+                aria-controls={`${title}-items`}
+            >
+                {`${title} (${count})`} <i className="fas fa-angle-down" />
+            </button>
+            <div className='collapse' id={`${title}-items`}>
+                <input type='checkbox' className='form-check-input' id={`${title}-item`} />
+            </div>
+        </React.Fragment>
     )
 }
--- client/src/pages/SearchResults.js
+++ client/src/pages/SearchResults.js
@@ -8,19 +8,19 @@ import ResultsContainer from '../components/ResultsContainer';
 export default function SearchResults() {
 
     const fakeFilterData = [
-        { title: 'school', count: 1 },
-        { title: 'department', count: 1 },
-        { title: 'level', count: 3 },
-        { title: 'term', count: 3 },
-        { title: 'status', count: 4 },
-        { title: 'day', count: 7 },
-        { title: 'instructor', count: 28 }
+        { title: 'school', count: 1, items: [{ name: 'INF', count: 28 }] },
+        { title: 'department', count: 1, items: [{ name: 'NA', count: 28 }] },
+        { title: 'level', count: 3, items: [{ name: 'LVL 1', count: 2 }, { name: 'LVL 2', count: 1 }, { name: 'LVL 3', count: 3 }] },
+        { title: 'term', count: 3, items: [{ name: '2019 Fall', count: 2 }, { name: '2019 Winter', count: 1 }, { name: '2020 Spring', count: 3 }] },
+        { title: 'status', count: 4, items: [{ name: 'Open', count: 2 }, { name: 'Open; reserved', count: 1 }, { name: 'Closed', count: 3 }, { name: 'Cancelled', count: 4 }] },
+        { title: 'day', count: 7, items: [{ name: 'Sunday', count: 2 }, { name: 'Monday', count: 1 }, { name: 'Tuesday', count: 3 }, { name: 'Wednesday', count: 4 }, { name: 'Thursday', count: 4 }, { name: 'Friday', count: 4 }, { name: 'Saturday', count: 4 }] },
+        { title: 'instructor', count: 28, items: [{ name: 'John Watson', count: 3 }]  }
     ];
 
     const backLinkData = { text: 'Start a new search', path: '/' };
 
     return (
-        <div className='container-fluid'>
+        <div className='container-fluid search-results-container'>
             <div className='row'>
                 <div className='col col-sm-2'>
                     <span className='label-small'>filters</span>
