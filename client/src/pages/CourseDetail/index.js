ba48c090ff52abddc1a8db1b2796f6a3f8164b6e
Alvin Galit <aroblesgalit@gmail.com>
2020-10-08 20:26:38 -0500

    Added description to course doc

--- client/src/pages/CourseDetail/index.js
+++ client/src/pages/CourseDetail/index.js
@@ -8,7 +8,7 @@ export default function CourseDetail() {
     function return5stars() {
         const stars = [];
         for (let i = 0; i < 5; i++) {
-            stars[i] = (<i className='fas fa-star' />);
+            stars[i] = (<i className='fas fa-star' key={i} />);
         }
         return stars;
     }
@@ -31,21 +31,30 @@ export default function CourseDetail() {
                         <div className='col-auto course-school'>INF 385T</div>
                         <div className='col course-instructor'>John Watson</div>
                     </div>
-                    <div className='row mt-3'>
-                        <div className='col'>Unique: 27185</div>
-                        <div className='col d-flex align-items-center'>
-                            Class Capacity: 
+                    <div className='row mt-2 align-items-end'>
+                        <div className='col-auto'>Unique: 27185</div>
+                        <div className='col-auto d-flex align-items-center'>
+                            Class Capacity:
                             <div className='capacity-bar'>
                                 <div className='capacity-count'></div>
                             </div>
-                            11/20    
+                            11/20
                         </div>
                         <div className='col d-flex align-items-end'>
                             Review:
                             <div className='review-stars'>
-                                { return5stars().map(star => star) }
+                                {return5stars().map(star => star)}
                             </div>
                             5/5 {`(10)`}
+                        </div>
+                    </div>
+                    <div className='row mt-4'>
+                        <div className='col-auto course-doc-label'> Description:</div>
+                        <div className='col'>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            commodo consequat.
                         </div>
                     </div>
                 </div>
