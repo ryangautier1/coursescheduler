27b36b53a0b8edf2bef52c110ab6607661b06239
Alvin Galit <aroblesgalit@gmail.com>
2020-10-09 18:04:18 -0500

    Finished the left column of instructor info

--- client/src/components/LabelAndValue/index.js
+++ client/src/components/LabelAndValue/index.js
@@ -3,8 +3,8 @@ import './labelAndValue.css';
 
 export default function LabelAndValue({ label, value }) {
     return (
-        <div className='label-value'>
-            <span className='label'>{label}: </span> {label === 'E-Mail' ? (
+        <div className='label-value mt-2'>
+            <span className='label'>{label}: </span> {label.toLowerCase() === 'e-mail' ? (
                 <a href={`mailto: ${value}`}>{value}</a>
             ) : value}
         </div>
