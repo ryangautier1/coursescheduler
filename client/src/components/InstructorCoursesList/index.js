940f1936498ecc5e46a791640b4c0d994682f11e
Alvin Galit <aroblesgalit@gmail.com>
2020-10-09 20:38:59 -0500

    Styled course list

--- client/src/components/InstructorCoursesList/index.js
+++ client/src/components/InstructorCoursesList/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './instructorCoursesList.css';
 
 export default function InstructorCoursesList() {
     return (
-        <div className='row'>
+        <div className='row mt-4 courses-list'>
             <div className='col'>
-                <div className='row'>
-                    <div className='col'>Current Courses: 1</div>
+                <div className='row mb-1'>
+                    <div className='col label-count'>Current Courses: 1</div>
                 </div>
-                <div className='row'>
-                    <div className='col'>INF 385T</div>
-                    <div className='col'>Virtual Environment</div>
-                    <div className='col'>2019 Fall</div>
-                    <div className='col'>View Course</div>
+                <div className='row py-1 course-item'>
+                    <div className='col-md-2'>INF 385T</div>
+                    <div className='col-md-4'>Virtual Environment</div>
+                    <div className='col-md-4'>2019 Fall</div>
+                    <div className='col-md-2'>
+                        <Link to='/course-detail'>View Course</Link>
+                    </div>
                 </div>
             </div>
         </div>
