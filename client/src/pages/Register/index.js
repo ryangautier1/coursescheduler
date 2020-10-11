e2984fb2abc1d8b3332e4115c061b196b75ce2eb
Alvin Galit <aroblesgalit@gmail.com>
2020-10-11 13:14:10 -0500

    Created Register page

---
+++ client/src/pages/Register/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../pages.css';
+import './register.css';
+
+export default function Register() {
+    return (
+        <div>
+            Register
+        </div>
+    )
+}
