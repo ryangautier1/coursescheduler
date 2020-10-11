5b8ad3e6b8d394821eaf3bfeaeeda3280eea5b6d
Alvin Galit <aroblesgalit@gmail.com>
2020-10-11 14:35:28 -0500

    Added form for course numbers

--- client/src/components/RegisterForm/index.js
+++ client/src/components/RegisterForm/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './registerForm.css';
+import ButtonPrimary from '../ButtonPrimary';
 
 export default function RegisterForm() {
     return (
@@ -9,7 +10,31 @@ export default function RegisterForm() {
                 <a className='list-group-item list-group-item-action flex-fill d-flex justify-content-center align-items-center' id='list-planner-list' data-toggle='list' href='#list-planner' role='tab' aria-controls='planner'>By Planner</a>
             </div>
             <div className='tab-content' id='nav-tabContent'>
-                <div className='tab-pane fade show active' id='list-courseNum' role='tabpanel' aria-labelledby='list-courseNum-list'>Course Number Form...</div>
+                <div className='tab-pane fade show active' id='list-courseNum' role='tabpanel' aria-labelledby='list-courseNum-list'>
+                    <form className='pt-5 pb-3 px-4'>
+                        <div className='form-row'>
+                            <div className='form-group col'>
+                                <label>Course Number</label>
+                                <input type='text' className='form-control' id='course1' />
+                            </div>
+                        </div>
+                        <div className='form-row'>
+                            <div className='form-group col'>
+                                <input type='text' className='form-control' id='course2' />
+                            </div>
+                        </div>
+                        <div className='form-row'>
+                            <div className='form-group col'>
+                                <input type='text' className='form-control' id='course3' />
+                            </div>
+                        </div>
+                        <div className='form-row'>
+                            <div className='form-group col d-flex justify-content-end'>
+                                <ButtonPrimary text='Register' submit />
+                            </div>
+                        </div>
+                    </form>
+                </div>
                 <div className='tab-pane fade' id='list-planner' role='tabpanel' aria-labelledby='list-planner-list'>Planner Form...</div>
             </div>
         </div>
