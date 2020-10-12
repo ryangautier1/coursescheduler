9ae78267a56f2687d068e78c62427e2a7dc7e066
Alvin Galit <aroblesgalit@gmail.com>
2020-10-12 15:35:24 -0500

    Fixed issue with form margin

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -6,7 +6,7 @@ import RegisterForm from '../../components/RegisterForm';
 
 export default function Register() {
     return (
-        <div className='container-fluid'>
+        <div className='container register-container'>
             <div className='row mt-5 justify-content-center'>
                 <div className='col col-md-10 col-lg-6'>
                     <PageTitle text='Register' />
