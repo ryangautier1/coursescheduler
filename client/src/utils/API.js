4f63a099e660231d7e26e8651db1b9b4201502a7
Alvin Galit <aroblesgalit@gmail.com>
2020-10-20 13:26:32 -0500

    Added login api to API file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 export default {
-    signup: function(data) {
+    signup: function (data) {
         return axios.post('/api/user-login/signup', data);
+    },
+    login: function (data) {
+        return axios.post('/api/user-login/login', data);
     }
 };
