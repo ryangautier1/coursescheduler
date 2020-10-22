474c09c78cb2cdb59339c3adc5c42d0c9703180f
Ryan Gautier <ryangautier2@gmail.com>
2020-10-22 18:47:29 -0500

    map courses in table

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -3,11 +3,31 @@ import './dashboard.css';
 
 function Dashboard() {
   const [semesterState, setSemesterState] = useState("Spring 2021");
+  const courses = [
+    {
+      name: "ITD 380",
+      title: "Intro Design",
+      room: "ABC Room",
+      instructor: "A.B.",
+      days: ["M", "W", "F"],
+      startTime: "8:30:00",
+      endTime: "10:30:00"
+    },
+    {
+      name: "ITD 380",
+      title: "Intro Design",
+      room: "ABC Room",
+      instructor: "A.B.",
+      days: ["M", "W", "F"],
+      startTime: "8:30:00",
+      endTime: "10:30:00"
+    }
+  ];
   const toggleDashboardSection = (e) => {
     e.target.classList.toggle("fa-arrow-down");
     e.target.classList.toggle("fa-arrow-up");
     let toggleId = e.target.getAttribute("data-section");
-    document.getElementById(toggleId).classList.toggle("d-none");
+    // document.getElementById(toggleId).classList.toggle("d-none");
   };
 
   return (
@@ -19,44 +39,43 @@ function Dashboard() {
         <h5 className="semester-heading">{semesterState}</h5>
         <div className="dashboard-section">
           {/* the data-section attribute should match the id of the section to be toggled */}
-          <i class="fas fa-arrow-down dashboard-section-arrow"
+          <i className="fas fa-arrow-down dashboard-section-arrow"
             onClick={(e) => { toggleDashboardSection(e) }}
-            data-section="dashboard-schedule"></i>
+            data-section="dashboard-schedule"
+            data-toggle="collapse"
+            data-target="#dashboard-schedule"
+            aria-expanded="false" aria-controls="dashboard-schedule">
+          </i>
           <h4 className="dashboard-section-heading d-inline-block">My Schedule</h4>
-          <div className="d-none" id="dashboard-schedule">
+          <div className="collapse" id="dashboard-schedule">
             <table className="table table-sm">
               <thead>
                 <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">First</th>
-                  <th scope="col">Last</th>
-                  <th scope="col">Handle</th>
+                  <th scope="col">COURSE</th>
+                  <th scope="col">TITLE</th>
+                  <th scope="col">ROOM</th>
+                  <th scope="col">INSTRUCTOR</th>
+                  <th scope="col">DAY & TIME</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td colspan="2">Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr>
+                {courses.map((course) => {
+                  return (
+                    <tr>
+                      <th scope="row">{course.name}</th>
+                      <td>{course.title}</td>
+                      <td>{course.room}</td>
+                      <td>{course.instructor}</td>
+                      <td>{course.days} {course.startTime}-{course.endTime}</td>
+                    </tr>
+                  )
+                })}
               </tbody>
             </table>
           </div>
         </div>
-        <div className="dashboard-section"><i class="fas fa-arrow-down dashboard-section-arrow" onClick={(e) => { toggleDashboardSection(e) }}></i>My Waitlist</div>
-        <div className="dashboard-section"><i class="fas fa-arrow-down dashboard-section-arrow" onClick={(e) => { toggleDashboardSection(e) }}></i>My Planner</div>
+        <div className="dashboard-section"><i className="fas fa-arrow-down dashboard-section-arrow" onClick={(e) => { toggleDashboardSection(e) }}></i>My Waitlist</div>
+        <div className="dashboard-section"><i className="fas fa-arrow-down dashboard-section-arrow" onClick={(e) => { toggleDashboardSection(e) }}></i>My Planner</div>
       </div>
       <div className="col-md-3 col-12">
         <h3 className="font-weight-light">DASHBOARD</h3>
