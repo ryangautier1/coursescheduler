13757adabba8b57ace120a986137fde3d8115410
Alvin Galit <aroblesgalit@gmail.com>
2020-11-03 17:51:44 -0600

    Created function in CourseContext that fetches all courses

--- client/src/utils/CourseContext.js
+++ client/src/utils/CourseContext.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import API from './API';
 
 const CourseContext = React.createContext();
 
 // Provider
 function CourseProvider(props) {
+
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        fetchCourses();
+    }, [])
+
+    // Fetch all courses
+    function fetchCourses() {
+        API
+            .fetchCourses()
+            .then(res => {
+                setCourses(res.data);
+                console.log(res.data);
+            })
+            .catch(err => console.error(err));
+    };
+
     return (
         <CourseContext.Provider
             value={{
-                
+
             }}
         >
             {props.children}
